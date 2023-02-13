fce4d7083d13c3ac3d9e7eee79c2999a0f752ec5
h0tcatSub <h0tcatnohotmensh0tcat@gmail.com>
2023-02-13 19:02:36 +0900

    add score

--- learn.py
+++ learn.py
@@ -21,6 +21,7 @@ bonus_s = list(map(int, bonus_s))
 result[0] = bonus_s[0]
 result[1] = bonus_s[1]
 result[2] = int(result[2])
-print("[BB回数予想  RB回数予想] [BB確率予想  RB確率予想] 合成確率")
+print("[BB回数予想  RB回数予想] 最大差玉 [BB確率予想  RB確率予想] 合成確率")
 merged_percent = (result[3] * result[4]) / ( result[3]+ result[4])
-print(result[0:2], f"[1/{result[3]}, ", f"1/{result[4]}]", f"1/{merged_percent}")
+print(result[0:2], result[2], f"[1/{result[3]}, ", f"1/{result[4]}]", f"1/{merged_percent}")
+print(f"Score : {reg.score(X, y)}")
